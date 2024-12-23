d4cb1083fde3fe6f0c0c2cd30f320ba1bf801a5d
CG <chris@carnal0wnage.com>
2024-12-22 22:18:20 -0500

    add some checks for open service desk and open signup

--- JIRAya.py
+++ JIRAya.py
@@ -20,8 +20,7 @@ colorama.init()
 #urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def banner():
-    if random.randint(0, 1) == 0:
-        print(f"""
+    print(f"""
                █████ █████ ███████████     █████████   ██ ██ █████ █████   █████████  
               ░░███ ░░███ ░░███░░░░░███   ███░░░░░███ ░██░██░░███ ░░███   ███░░░░░███ 
                ░███  ░███  ░███    ░███  ░███    ░███ ░░ ░░  ░░███ ███   ░███    ░███ 
@@ -32,32 +31,6 @@ def banner():
          ░░░░░░░░   ░░░░░ ░░░░░   ░░░░░ ░░░░░   ░░░░░          ░░░░░    ░░░░░   ░░░░░ 
                     JIRA Yet Another vulnerability Analyzer by @FR13ND0x7f & carnal0wnage
          """)
-    else:    
-        print(f"""
-
-                                           -     ^ ╚▀┌ ,
-                                        ▐█P     ▄    `██`
-                                        ▐█  ▀ ███▀█r"  █▌
-                                        ▐▌, ▀▄▐████ ═  ▐█      Y
-                            ,▄▄   ¿     ██▄▄▀       ,▄▄██     , ▀,,
-                       ▄▄▀▀███▄█,█      █▀████▀▀▀▀███████      █ ▀▄█████▄,
-                          ▄██████`      █▄▓▌⌐█▌  ██▌═▀▄██ ,     █▄████▄  -"
-                        ╔███████▌     ▐ ██       █    ███ █     ▐███████▄
-                       ▄████████ ▌    ▐▌█▌       ▌     ██ █      ████████▄
-                      ▀▀▀ ▄████▌█▌   ▄███⌐    ▄███     ██▐█ ▄   █⌐████▄`▀██
-                         ▄████████  ▐████▌             █▌██▄█  ▐██▐████▄
-                        ▄█████████▌ █████▌     ,,,,  ╚ ██████  ███▌█████▄
-                       ▄█▀█████████▐██████▄          ▄███████▌▐████████▀██
-                      `  ▐███████████████████▄▄▄▄▄▄████████████████████▌ `▀
-                        ╒██▀██▀▀` █████████████████████████████▌ -▀▀▀▀██▄
-                        ▀▀        █▀████ ▌▀█  "▀▀▀  ,███ ▐█████'
-                                  ▐ ███-  ▄▄▄▄▄▄▄▄▄▄███  `███`█
-                                     ██`   ▀▀▀▀▀▀▀▀▀▀▀    ███
-                                     ██-    (`╓  <  "     ██
-                                      ▀▀     '            ▀▀
-                                            JIRA"YA    
-                     JIRA Yet Another vulnerability Analyzer by @FR13ND0x7f
-        """)
 
 def JIRA_TestCases(url):
     vulnerabilities = []
@@ -73,7 +46,7 @@ def JIRA_TestCases(url):
     #print ("+ Using collaborator as:", collaborator)
     #collaborator = f"https://victomhost:1337@example.com" #ask user for collaborator URL
     #cve20198451 = f"{url}/plugins/servlet/gadgets/makeRequest?url={collaborator}" #/plugins/servlet/gadgets/makeRequest?url=
-    CVE201911581 = f"{url}secure/ContactAdministrators!default.jspa"
+    contact_admin_url = f"{url}secure/ContactAdministrators!default.jspa"
     CVE201820824 = f"{url}plugins/servlet/Wallboard/?dashboardId=10000&dashboardId=10000&cyclePeriod=alert(document.domain)"
     cve202014179 = f"{url}secure/QueryComponent!Default.jspa"
     cve202014181 = f"{url}ViewUserHover.jspa?username=Admin"
@@ -90,6 +63,8 @@ def JIRA_TestCases(url):
     cve20220540 = f"{url}InsightPluginShowGeneralConfiguration.jspa;"
     cve202205401 = f"{url}secure/WBSGanttManageScheduleJobAction.jspa;"
     uaed = f"{url}secure/popups/UserPickerBrowser.jspa"
+    service_desk_url = f"{url}servicedesk/customer/user/login"
+    signup_url = f"{url}secure/Signup!default.jspa"
 
     # Check for unauthenticated access to JIRA dashboards
 
@@ -129,7 +104,7 @@ def JIRA_TestCases(url):
             else:
                 print("    No dashboards found.")
         else:
-            print(f"{Fore.YELLOW}- No unauthenticated access to JIRA dashboards detected on: {dashboard_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}\n- No unauthenticated access to JIRA dashboards detected on: {dashboard_url}{Style.RESET_ALL}")
     except json.JSONDecodeError:
         print(f"{Fore.RED}- Failed to parse JSON response from: {dashboard_url}{Style.RESET_ALL}")
     except Exception as e:
@@ -168,7 +143,7 @@ def JIRA_TestCases(url):
             else:
                 print("    No project categories found.")
         else:
-            print(f"{Fore.YELLOW}- No unauthenticated access to JIRA project categories detected on: {project_category_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}\n- No unauthenticated access to JIRA project categories detected on: {project_category_url}{Style.RESET_ALL}")
     except json.JSONDecodeError:
         print(f"{Fore.RED}- Failed to parse JSON response from: {project_category_url}{Style.RESET_ALL}")
     except Exception as e:
@@ -230,7 +205,7 @@ def JIRA_TestCases(url):
             else:
                 print("    No admin projects found.")
         else:
-            print(f"{Fore.YELLOW}- No unauthenticated access to JIRA admin projects detected on: {admin_projects_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}\n- No unauthenticated access to JIRA admin projects detected on: {admin_projects_url}{Style.RESET_ALL}")
     except json.JSONDecodeError:
         print(f"{Fore.RED}- Failed to parse JSON response from: {admin_projects_url}{Style.RESET_ALL}")
     except Exception as e:
@@ -284,7 +259,7 @@ def JIRA_TestCases(url):
             print(f"  Header: {header}")
             print(f"  User Details: {users if users else 'No users listed.'}")
         else:
-            print(f"{Fore.YELLOW}- No CVE-2019-3403 vulnerability detected on: {user_picker_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}\n- No CVE-2019-3403 vulnerability detected on: {user_picker_url}{Style.RESET_ALL}")
     except json.JSONDecodeError:
         print(f"{Fore.RED}- Failed to parse JSON response from: {user_picker_url}{Style.RESET_ALL}")
     except Exception as e:
@@ -321,12 +296,15 @@ def JIRA_TestCases(url):
             print(f"  Total Groups Found: {total_groups}")
             print(f"  Group Header: {group_header}")
             print(f"  Group Details: {groups if groups else 'No groups listed.'}")
+        elif response.status_code == 403:
+            print(f"{Fore.YELLOW}\n- No CVE-2019-8449 vulnerability detected on: {JRASERVER_url}{Style.RESET_ALL}")
+
         else:
-            print(f"{Fore.YELLOW}- No CVE-2019-8449 vulnerability detected on: {JRASERVER_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}\n- No CVE-2019-8449 vulnerability detected on: {JRASERVER_url}{Style.RESET_ALL}")
     except json.JSONDecodeError:
         print(f"{Fore.RED}- Failed to parse JSON response from: {JRASERVER_url}{Style.RESET_ALL}")
     except Exception as e:
-        print(f"{Fore.RED}- An error occurred while checking {JRASERVER_url}: {e}{Style.RESET_ALL}")
+        print(f"{Fore.RED}- An error occurred while checking {JRASERVER_url} error:{e}{Style.RESET_ALL}")
  
     check_cve_2019_8449(url)
 
@@ -338,13 +316,124 @@ def JIRA_TestCases(url):
     except:
         pass
 
-    #RCE Jira=CVE-2019–11581
+    def check_open_servicedeck(url):
+        '''
+        Checks for open service desk. attempt to signup
+        '''
+        service_desk_url = f"{url}servicedesk/customer/user/login"
+        
     try:
-        response = requests.get(CVE201911581, verify=False)
-        if response.status_code == 200 in response.text:
-            vulnerabilities.append(f"+ CVE-2019–11581 [Potential RCE] : Need to exploit manully for now : https://hackerone.com/reports/706841 | URL : {CVE201911581}")
-    except:
-        pass        
+        print(f"{Fore.YELLOW}\nINFO: IN DEVELOPMENT - Open Service Desk Signup")
+        response = requests.get(service_desk_url, allow_redirects=False, verify=False)
+
+        # Check for the vulnerability
+        if response.status_code == 200:
+            response_text = response.text
+            # print(response_text) #DEBUG
+
+
+            vulnerabilities.append(f"+ Open Service Desk Signup Found: Manual exploitation required [try to signup and log in] | URL: {service_desk_url}")
+            print(f"\n{Fore.GREEN}+ Open Service Desk Signup Found: Manual exploitation required [try to signup and log in]{Style.RESET_ALL}")
+            print(f"  URL: {service_desk_url}")
+            print(f"  Note: Exploitation requires manual steps.")
+            print(f"  Note: Refer to: https://medium.com/@intideceukelaire/hundreds-of-internal-servicedesks-exposed-due-to-covid-19-ecd0baec87bd")
+        elif response.status_code == 403:
+            print(f"{Fore.YELLOW}\n- No Open Service Desk vulnerability detected on: {service_desk_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}- HTTP Status Code: {response.status_code}{Style.RESET_ALL}")
+        elif response.status_code == 302:
+            location = response.headers.get("Location", "No Location header found") 
+            print(f"{Fore.YELLOW}- Redirection Detected (302) for: {service_desk_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}- Location Header: {location}")
+            print(f"{Fore.YELLOW}- This program doesnt follow 302 - Try: curl -k -v \'{service_desk_url}\'{Style.RESET_ALL}")
+        else:
+            print(f"{Fore.YELLOW}\n- No Open Service Desk vulnerability detected on: {service_desk_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}- HTTP Status Code: {response.status_code}{Style.RESET_ALL}")
+
+    except Exception as e:
+            print(f"{Fore.RED}*  An error occurred while checking {service_desk_url}: {e}{Style.RESET_ALL}")
+
+    check_open_servicedeck(url)
+
+    def check_open_signup(url):
+        '''
+        Checks for open service desk. attempt to signup
+        '''
+        signup_url = f"{url}secure/Signup!default.jspa"
+
+    try:
+        print(f"{Fore.YELLOW}\nINFO: IN DEVELOPMENT - Open JIRA Signup")
+        response = requests.get(signup_url, allow_redirects=False, verify=False)
+
+        # Check for the vulnerability
+        if response.status_code == 200:
+            response_text = response.text
+            # print(response_text) #DEBUG
+            if ("Sorry, you can&#39;t sign up to this Jira site" in response_text or "No puede registrarse" in response_text):
+                print(f"{Fore.YELLOW}\n- No Open Signup vulnerability detected on: {signup_url}") 
+                # print(f"  URL: {contact_admin_url}")
+            else:
+ 
+                vulnerabilities.append(f"+ Open Signup Page: Manual exploitation required [try to signup and log in] | URL: {signup_url}")
+                print(f"\n{Fore.GREEN}+ Open Signup Page Found: Manual exploitation required [try to signup and log in]{Style.RESET_ALL}")
+                print(f"  URL: {signup_url}")
+                print(f"  Note: Exploitation requires manual steps.")
+                # print(f"  Note:Refer to: https://medium.com/@intideceukelaire/hundreds-of-internal-servicedesks-exposed-due-to-covid-19-ecd0baec87bd")
+        elif response.status_code == 403:
+            print(f"{Fore.YELLOW}\n- No Open Signup vulnerability detected on: {signup_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}- HTTP Status Code: {response.status_code}{Style.RESET_ALL}")
+        elif response.status_code == 302:
+            location = response.headers.get("Location", "No Location header found") 
+            print(f"{Fore.YELLOW}- Redirection Detected (302) for: {signup_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}- Location Header: {location}")
+            print(f"{Fore.YELLOW}- This program doesnt follow 302 - Try: curl -k -v \'{signup_url}\'{Style.RESET_ALL}")
+        else:
+            print(f"{Fore.YELLOW}\n- No Open Signup vulnerability detected on: {signup_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}- HTTP Status Code: {response.status_code}{Style.RESET_ALL}")
+
+    except Exception as e:
+            print(f"{Fore.RED}*  An error occurred while checking {signup_url}: {e}{Style.RESET_ALL}")
+
+    check_open_signup(url)
+
+    def check_cve_2019_11581(url):
+        '''
+        Checks for CVE-2019-11581, a potential Remote Code Execution vulnerability in Jira.
+        '''
+        contact_admin_url = f"{url}secure/ContactAdministrators!default.jspa"
+
+    try:
+        response = requests.get(contact_admin_url, verify=False)
+
+        # Check for the vulnerability
+        if response.status_code == 200:
+            response_text = response.text
+            # print(response_text) DEBUG
+
+            if ("administrator has not yet configured" in response_text or 
+                "no ha configurado" in response_text or "noch nicht konfiguriert" in response_text or "не настроил эту контактную форму" in response_text or "管理员尚未配置此联系表" in response_text):
+                print(f"{Fore.YELLOW}\n- No CVE-2019-11581 vulnerability detected on: {contact_admin_url}") 
+                print(f"{Fore.YELLOW}\t **The contact form is not configured and most likely NOT vulnerable.**{Style.RESET_ALL}")
+                # print(f"  URL: {contact_admin_url}")
+            else:
+                vulnerabilities.append(f"+ CVE-2019-11581 [Potential RCE]: Manual exploitation required | URL: {contact_admin_url}")
+                print(f"\n{Fore.GREEN}+ CVE-2019-11581 Detected - The contact form is configured and potential vulnerable [MANUAL REVIEW REQUIRED]{Style.RESET_ALL}")
+                print(f"  URL: {contact_admin_url}")
+                print(f"  Note: Exploitation requires manual steps.")
+                print(f"  Note: For this issue to be exploitable at least one of the following conditions must be met:")
+                print(f"  1. An SMTP server has been configured in Jira and the Contact Administrators Form is enabled")
+                print(f"  2. or an SMTP server has been configured in Jira and an attacker has \"JIRA Administrators\" access.")
+                print(f"  Note:Refer to: https://jira.atlassian.com/browse/JRASERVER-69532 && https://hackerone.com/reports/706841")
+        elif response.status_code == 403:
+            print(f"{Fore.YELLOW}- HTTP Status Code: {response.status_code}")
+
+        else:
+            print(f"{Fore.YELLOW}- No CVE-2019-11581 vulnerability detected on: {contact_admin_url}{Style.RESET_ALL}")
+            print(f"{Fore.YELLOW}- HTTP Status Code: {response.status_code}{Style.RESET_ALL}")
+
+    except Exception as e:
+            print(f"{Fore.RED}* CVE-2019-11581 An error occurred while checking {contact_admin_url}: {e}{Style.RESET_ALL}")
+
+    check_cve_2019_11581(url)
 
     #cve-2018-20824
     try:
@@ -426,15 +515,15 @@ def JIRA_TestCases(url):
                 # Stream response to handle large files
                 response = requests.get(target_url, verify=False, allow_redirects=False, stream=True)
                 print(f"{Fore.YELLOW}- HTTP Status Code: {response.status_code}")
+
+                contains_dependency = False  # Flag to check for the keyword
             
-                if response.status_code == 200:
-                    # Check content for "dependency" keyword
-                    contains_dependency = False
-                    for chunk in response.iter_lines(decode_unicode=True):
-                        if "dependency" in chunk:
-                            contains_dependency = True
-                            break  # Stop further processing if keyword is found
+                for chunk in response.iter_lines(decode_unicode=True):
+                    if response.status_code == 200 and "dependency" in chunk:
+                        contains_dependency = True
+                        break  # Stop further processing if keyword is found
                 
+                if response.status_code == 200:
                     if contains_dependency:
                         vulnerabilities.append(f"+ CVE-2019-8442 [Information Disclosure] : https://jira.atlassian.com/browse/JRASERVER-69241 visit the affected url,the server will leaking some server's information | URL : {target_url}")
                         print(f"{Fore.GREEN}+ CVE-2019-8442 Detected: Information Disclosure vulnerability found!{Style.RESET_ALL}")
@@ -442,9 +531,12 @@ def JIRA_TestCases(url):
                         #print(f"  Visit the URL for more details: https://jira.atlassian.com/browse/JRASERVER-69241")
                     else:
                         print(f"{Fore.BLUE}- NEEDS MANUAL REVIEW - No sensitive information detected at {target_url}{Style.RESET_ALL}")
+                    
                 elif response.status_code == 302:
-                    print(f"{Fore.YELLOW}- Possible Redirection Detected (302): {target_url}{Style.RESET_ALL}")
-                    print(f"  Response text: {response.text}")
+                    location = response.headers.get("Location", "No Location header found") 
+                    print(f"{Fore.YELLOW}- Redirection Detected (302) for: {target_url}{Style.RESET_ALL}")
+                    print(f"{Fore.YELLOW}- Location Header: {location}")
+                    print(f"{Fore.YELLOW}- This program doesnt follow 302 - Try: curl -k -v \'{target_url}\'{Style.RESET_ALL}")
                 else:
                     print(f"{Fore.BLUE}- No vulnerability detected at {target_url}{Style.RESET_ALL}")
             except Exception as e:
@@ -527,12 +619,16 @@ def parse_jira_response(response_text):
         print(f"Error parsing JSON response: {e}")
 
 def check_jira(url, path):
-    if not url.startswith("http") or url.startswith("https"):
-        url = "https://" + url
-    else:
-        url = url
-        print(f"{Fore.YELLOW}[Scanning] : " + url + f"{Style.RESET_ALL}")
-        #print(url)
+    # Parse the URL to check its components
+    parsed_url = urlparse(url)
+
+    if not parsed_url.scheme:  # No scheme provided (e.g., just an IP or domain)
+        url = "https://" + url  # Default to https:// if no scheme is provided
+    else:  # Scheme is present (e.g., http://example.com or https://example.com)
+        url = f"{parsed_url.scheme}://{parsed_url.netloc}"  # Keep the provided scheme and netloc
+
+    print(f"{Fore.YELLOW}[Scanning] : {url}{Style.RESET_ALL}")
+
 
     try:
         full_url = url + path
@@ -579,8 +675,7 @@ def main():
 
     if args.single:
         check_jira(args.single, args.path)
-        # print(args.single)
-        # print(args.path)
+
     elif args.TheTimeMachine:
         url = f'https://web.archive.org/cdx/search/cdx?url=*.{args.TheTimeMachine}/*&output=txt&fl=original&collapse=urlkey&page=/'
         print(f"\nTarget Loaded: "+args.TheTimeMachine)
